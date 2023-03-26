419ef1d1bc79d644acee50eed89c67ab49c899ba
Keith Price <keithprice.dev@gmail.com>
2023-03-26 11:35:11 +0700

    added logo to header footer and as favicon

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,5 +1,5 @@
 export default {
-	title: `John Irwin | Planning and Application Services`,
+	title: `Planning and Application Services`,
 	description: `We provide a personal and bespoke planning application service for you which has been created to ensure a smooth and seamless process to obtain your planning approval.`,
 	url: `http://www.planningapplicationservices.co.uk/`, // this should link back to the website
 	name: `Planning Application Services`, // The short name of the business or brand name. Used for things like the copyright in the footer.
