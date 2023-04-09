4f5d02aeaf60528c0cd728107180bb3062dc5b99
Keith Price <keithprice.dev@gmail.com>
2023-04-09 11:30:40 +0700

    removed unused files and changed site url in astro build

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
 	title: `Planning and Application Services`,
 	description: `We provide a personal and bespoke planning application service for you which has been created to ensure a smooth and seamless process to obtain your planning approval.`,
-	url: `http://www.planningapplicationservices.co.uk/`, // this should link back to the website
+	url: `http://www.planningapplicationservices.com/`, // this should link back to the website
 	name: `Planning Application Services`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: false,
 	showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
